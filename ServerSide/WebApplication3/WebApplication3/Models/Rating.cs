0b31f2586a192e50d346bef487a672be716d4c5d
ori12396 <93042166+ori12396@users.noreply.github.com>
2022-05-31 18:50:29 +0300

    Add files via upload

---
+++ ServerSide/WebApplication3/WebApplication3/Models/Rating.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+       
+        [Range(1, 5)]
+        public int Rate { get; set; }
+      
+        public string Name { get; set; }
+        [RegularExpression("^[a-zA-Z]*$")]
+        [MaxLength(50)]
+        public string Feedback { get; set; }
+        
+        public string? Date { get; set; }
+        public string? Time { get; set; }
+
+    }
+}
